633f841281bf0b09947cc332bbbf34a30931391d
GhalaMalbishri <s443007832@st.uqu.edu.sa>
2024-07-18 11:11:37 +0300

    hh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,12 +10,12 @@ const routes: Routes = [
     redirectTo:'./auth/login',
     pathMatch:'full'
   },
- 
+
   {
     path:'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   },
-
+//
   {
     path:'apps', component:MainLayoutComponent,
     loadChildren: () => import('./main-apps/main-apps.module').then(m => m.MainAppsModule)

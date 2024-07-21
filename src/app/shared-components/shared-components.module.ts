c12d3b711086b72cafcd1fa5e79cc0846293c123
GhalaMalbishri <s443007832@st.uqu.edu.sa>
2024-07-21 10:28:58 +0300

    added styles , customerhome, customerView-rest, imgs

--- src/app/shared-components/shared-components.module.ts
+++ src/app/shared-components/shared-components.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { InputTextComponent } from './input-text/input-text.component';
 import { RestaurantCardComponent } from './restaurant-card/restaurant-card.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -15,7 +16,15 @@ import { RestaurantCardComponent } from './restaurant-card/restaurant-card.compo
     RestaurantCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule // added this for ?
+  ],
+  //added this:
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  exports: [
+    RestaurantCardComponent
   ]
+
+
 })
 export class SharedComponentsModule { }
